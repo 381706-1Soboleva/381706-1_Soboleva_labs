8a530233bbaeb6d3970fa6a6f7deb95c04529271
381706-1Soboleva <43166066+381706-1Soboleva@users.noreply.github.com>
2018-12-01 19:46:58 +0300

    Update mainVector.cpp

--- Vector/mainVector.cpp
+++ Vector/mainVector.cpp
@@ -1,16 +1,1 @@
-#include "VectorLib.h"
 
-#include <iostream>
-
-/*using namespace std;
-void main()
-{
-	TVector<int> A(3), B(A);
-	if (A==B) 
-		for (int i=0; i<3; i++)
-			A[i]=i;
-	cout<<A<<"\n";
-	B=A+1;
-	cout<<B<<"\n";
-	cout<<A*B<<"\n";
-}*/
