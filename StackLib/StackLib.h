9dce7218a3d2c8dc5b79ab44976a953db43081e1
381706-1Soboleva <juliasobolsky@yandex.ru>
2018-12-08 14:04:57 +0300

    Stack files

---
+++ StackLib/StackLib.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class TStack
+{
+protected:
+	int size;
+	T* m;
+	int top;
+
+public:
+	TStack<T> (int s = 0);
+	TStack<T> (TStack<T> &A);
+
+	void Put(T A);
+	T Get();
+	bool IsFull();
+	bool IsEmpty();
+};
+//-------------------------------------------------------------------------------------------------
+template <class T>
+TStack<T>::TStack(int s)
+{
+	if (s < 0)
+		throw -1;
+	else if (s == 0)
+	{
+		size = 0;
+		top = 0;
+		m = 0;
+	}
+	else
+	{
+		size = s;
+		top = 0;
+		m = new T [size];
+	}
+}
+//-------------------------------------------------------------------------------------------------
+template <class T>
+TStack<T>::TStack(TStack<T> &A)
+{
+	size = A.size;
+	top = A.top;
+	if (size == 0)
+		m = 0;
+	else
+	{
+		m = new T [size];
+		for (int i = 0; i < size; i++)
+			m[i] = A.m[i];
+	}
+}
+//-------------------------------------------------------------------------------------------------
+template <class T>
+void TStack<T>::Put(T A)
+{
+	if (IsFull())
+		throw -3;
+	else
+	{
+		m[top] = A;
+		top++;
+	}
+}
+//-------------------------------------------------------------------------------------------------
+template <class T>
+T TStack<T>::Get()
+{
+	if (IsEmpty())
+		throw -3;
+	else
+	{
+		top--;
+		return m[top];
+	}
+}
+//-------------------------------------------------------------------------------------------------
+template <class T>
+bool TStack<T>::IsFull()
+{
+	if (top == size)
+		return true;
+	else
+		return false;
+}
+//-------------------------------------------------------------------------------------------------
+template <class T>
+bool TStack<T>::IsEmpty()
+{
+	if (top == 0)
+		return true;
+	else
+		return false;
+}
