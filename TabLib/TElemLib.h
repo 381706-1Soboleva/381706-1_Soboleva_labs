fa067f2aa9b59b5b1b629de98006ff090654850a
381706-1Soboleva <juliasobolsky@yandex.ru>
2019-04-19 16:35:02 +0300

    design problem was solved

--- TabLib/TElemLib.h
+++ TabLib/TElemLib.h
@@ -7,35 +7,35 @@ class TElem
 {
 protected:
 
-	string key;
-	T data;
+  string key;
+  T data;
 
 public:
 
-	void SetKey(string _key);
-	void SetData(T _data);
-	string GetKey();
-	T GetData();
+  void SetKey(string _key);
+  void SetData(T _data);
+  string GetKey();
+  T GetData();
 
-	TElem (string _key = "", T _data = 0);
-	TElem (TElem &A);
-	TElem& operator = (TElem &A);
-	bool operator == (TElem &A);
-	bool operator != (TElem &A);
+  TElem (string _key = "", T _data = 0);
+  TElem (TElem &A);
+  TElem& operator = (TElem &A);
+  bool operator == (TElem &A);
+  bool operator != (TElem &A);
 
-	template <class T1>
-	friend istream & operator>>( istream &in, TElem<T1> &t)
+  template <class T1>
+  friend istream & operator>>( istream &in, TElem<T1> &t)
   {
     in>>t.key;
-		in>>t.data;
+    in>>t.data;
     return in;
   }
 
-	template <class T1>
+  template <class T1>
   friend ostream & operator<<( ostream &out, TElem<T1> &t)
   {    
-		out<<t.GetKey()<<"\t";
-		out<<t.GetData()<<"\t";
+    out<<t.GetKey()<<"\t";
+    out<<t.GetData()<<"\t";
     return out;
   }
 };
@@ -43,57 +43,57 @@ public:
 template <class T>
 void TElem<T>::SetKey(string _key)
 {
-	key = _key;
+  key = _key;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 void TElem<T>::SetData(T _data)
 {
-	data = _data;
+  data = _data;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 string TElem<T>::GetKey()
 {
-	return key;
+  return key;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 T TElem<T>::GetData()
 {
-	return data;
+  return data;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TElem<T>::TElem (string _key, T _data)
 {
-	key = _key;
-	data = _data;
+  key = _key;
+  data = _data;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TElem<T>::TElem(TElem &A)
 {
-	key = A.key;
-	data = A.data;
+  key = A.key;
+  data = A.data;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 TElem<T>& TElem<T>::operator = (TElem &A)
 {
-	key = A.key;
-	data = A.data;
-	return *this;
+  key = A.key;
+  data = A.data;
+  return *this;
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TElem<T>::operator == (TElem &A)
 {
-	return ((key == A.key) && (data == A.data));
+  return ((key == A.key) && (data == A.data));
 }
 //-------------------------------------------------------------------------------------------------
 template <class T>
 bool TElem<T>::operator != (TElem &A)
 {
-	return ((key != A.key) || (data != A.data));
+  return ((key != A.key) || (data != A.data));
 }
--- TabTest/test_tab.cpp
+++ TabTest/test_tab.cpp
@@ -38,15 +38,15 @@ TEST (TabLib, can_get_from_tab)
 {
   TTab<int> A(4);
   A.Put("kif", 98);
-	int k;
+  int k;
   ASSERT_NO_THROW (k = A["kif"]);
 }
 //-------------------------------------------------------------------------------------------------
 TEST(TabLib, can_delete_from_tab)
 {
   TTab<int> s(1);
-	s.Put("kig", 7);
-	ASSERT_NO_THROW (s.Del("kig"));
+  s.Put("kig", 7);
+  ASSERT_NO_THROW (s.Del("kig"));
 }
 //-------------------------------------------------------------------------------------------------
 TEST (TabLib, throws_when_delete_from_empty_Tab)
@@ -59,8 +59,8 @@ TEST (TabLib, get_gives_right_answer)
 {
   TTab<int> A(2);
   A.Put("bol", 8);
-	int k = A["bol"];
-	EXPECT_EQ (8, k);
+  int k = A["bol"];
+  EXPECT_EQ (8, k);
 }
 //-------------------------------------------------------------------------------------------------
 TEST (TabLib, isfull_gives_right_true_answer)
@@ -85,6 +85,6 @@ TEST (TabLib, isempty_gives_right_true_answer)
 TEST (TabLib, isempty_gives_right_false_answer)
 {
   TTab<int> A(1);
-	A.Put("kdgjh", 8);
+  A.Put("kdgjh", 8);
   EXPECT_EQ (false, A.IsEmpty());
 }
