36e384c99e97609069f70f8d817754eb1dc37471
381706-1Soboleva <43166066+381706-1Soboleva@users.noreply.github.com>
2019-05-08 17:41:57 +0300

    Update test_plex.cpp

--- PlexTest/test_plex.cpp
+++ PlexTest/test_plex.cpp
@@ -11,37 +11,38 @@ TEST(PointLib, can_create_default_point)
 //-------------------------------------------------------------------------------------------------
 TEST(PointLib, can_copy_point)
 {
-	TPoint s(2, 6);
+  TPoint s(2, 6);
   ASSERT_NO_THROW (TPoint s1(s));
 }
 //-------------------------------------------------------------------------------------------------
 TEST(PointLib, can_compare_different_points)
 {
-	TPoint s(2, 6), s1(3,8);
+  TPoint s(2, 6), s1(3,8);
   EXPECT_FALSE (s1 == s);
 }
 //-------------------------------------------------------------------------------------------------
 TEST(PointLib, can_compare_equal_points)
 {
-	TPoint s(2, 6), s1(2,6);
+  TPoint s(2, 6), s1(2,6);
   EXPECT_TRUE (s1 == s);
 }
 //-------------------------------------------------------------------------------------------------
 TEST(PointLib, can_assign_point)
 {
-	TPoint s(2, 6), s1(3,8);
+  TPoint s(2, 6), s1(3,8);
   ASSERT_NO_THROW (s1 = s);
 }
 //-------------------------------------------------------------------------------------------------
-TEST(SectionLib, can_create_default_section)
+TEST(SectionLib, can_create_section)
 {
-  ASSERT_NO_THROW (TSection s());
+  TPoint s(2, 6), s1(3,8);
+  ASSERT_NO_THROW (TSection h(s,s1));
 }
 //-------------------------------------------------------------------------------------------------
 TEST(SectionLib, can_copy_section)
 {
-	TPoint s(2, 6), s1(3,8);
-	TSection h(s, s1);
+  TPoint s(2, 6), s1(3,8);
+  TSection h(s, s1);
   ASSERT_NO_THROW (TSection l(h));
 }
 //-------------------------------------------------------------------------------------------------
@@ -52,13 +53,13 @@ TEST(PlexLib, can_create_default_plex)
 //-------------------------------------------------------------------------------------------------
 TEST (PlexLib, can_create_plex)
 {
-	TPoint A, B(7,4);
-	ASSERT_NO_THROW (TPlex P1(&A, &B));
+  TPoint A, B(7,4);
+  ASSERT_NO_THROW (TPlex P1(&A, &B));
 }
 //-------------------------------------------------------------------------------------------------
 TEST(PlexLib, can_copy_plex)
 {
-	TPoint A, B(7,4);
+  TPoint A, B(7,4);
   TPlex N(&A,&B);
   ASSERT_NO_THROW (TPlex F(N));
 }
@@ -66,23 +67,23 @@ TEST(PlexLib, can_copy_plex)
 TEST (PlexLib, can_search_point)
 {
   TPoint A, B(7,4);
-	TPoint *k;
-	TPlex s(&A,&B);
+  TPoint *k;
+  TPlex s(&A,&B);
   ASSERT_NO_THROW (k = (s.Search(&A)));
 }
 //-------------------------------------------------------------------------------------------------
 TEST (PlexLib, search_gives_right_answer)
 {
   TPoint A, B(7,4);
-	TPlex s(&A,&B);
-	TPoint k = *(s.Search(&A));
+  TPlex s(&A,&B);
+  TPoint k = *(s.Search(&A));
   EXPECT_TRUE (A == k);
 }
 //-------------------------------------------------------------------------------------------------
 TEST (PlexLib, can_add_point)
 {
   TPoint A, B(7,4), C(0,15);
-	TPlex P(&A, &B);
+  TPlex P(&A, &B);
   ASSERT_NO_THROW (P.Add(&A, &C));
 }
 
